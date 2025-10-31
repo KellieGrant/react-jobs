c4e6776a7b65d8620f239fcac4b66d485b60c537
KeltaDawn <kellieleagrant@gmail.com>
2025-10-31 07:57:05 -0400

    added functionality to the delete jobs

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import JobPage, { jobLoader } from './pages/JobPage';
 import AddJobPage from './pages/AddJobPage';
 
 const App = () => {
+   // Add New Job
    const addJob = async newJob => {
       const res = await fetch('/api/jobs', {
          method: 'POST',
@@ -23,12 +24,26 @@ const App = () => {
       return;
    };
 
+   //Delete Job
+   const deleteJob = async id => {
+      const res = await fetch(`/api/jobs/${id}`, {
+         method: 'DELETE',
+      });
+      return;
+   };
+
+   //Edit Job
+
    const router = createBrowserRouter(
       createRoutesFromElements(
          <Route path='/' element={<MainLayout />}>
             <Route index element={<HomePage />} />
             <Route path='/jobs' element={<JobsPage />} />
-            <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
+            <Route
+               path='/jobs/:id'
+               element={<JobPage deleteJob={deleteJob} />}
+               loader={jobLoader}
+            />
             <Route
                path='/add-job'
                element={<AddJobPage addJobSubmit={addJob} />}
